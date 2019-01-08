fb996d42d837f481024eb0279c18b9a9478e8f83
HanByulKang <hanstar@designme.ai>
2019-01-08 16:28:24 +0900

    EntityFrameworkCore 작업

--- TodayLunchAPI/Controllers/SelectAPIController.cs
+++ TodayLunchAPI/Controllers/SelectAPIController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using TodayLunchAPI.Models;
 using Microsoft.Extensions.Options;
 using System.Data.SqlClient;
 using System.Data;
--- TodayLunchAPI/TodayLaunchContext.cs
+++ TodayLunchAPI/TodayLaunchContext.cs
@@ -1,5 +1,4 @@
 ﻿using LunchCommon.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
