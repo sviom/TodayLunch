6caf4b34808bd2047d8a19860f801ead6f33fd84
HanByulKang <kanghanstar@outlook.com>
2019-01-08 21:23:05 +0900

    Launcher 생

---
+++ LunchLibrary/SqlLauncher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunchLibrary
+{
+    public static class SqlLauncher
+    {
+        public static bool Insert<T>(T inputObject) where T : class
+        {
+            bool result = false;
+            try
+            {
+                using (var db = new TodayLunchContext())
+                {
+                    db.Entry(inputObject).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+                    var set = db.Set<T>();
+                    set.Add(inputObject);
+                    int saveResult = db.SaveChanges();
+                    if (saveResult > 0)
+                        result = true;
+                }
+            }
+            catch
+            {
+            }
+
+            return result;
+        }
+    }
+}
--- LunchLibrary/TodayLunchContext.cs
+++ LunchLibrary/TodayLunchContext.cs
@@ -12,13 +12,13 @@ namespace LunchLibrary
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            // appsettings.json 파일 사용
             IConfiguration configuration = new ConfigurationBuilder()
-                .SetBasePath(Environment.CurrentDirectory) // Directory where the json files are located
+                .SetBasePath(Environment.CurrentDirectory)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .Build();
 
             optionsBuilder.UseSqlServer(configuration.GetConnectionString("lunchConnection"));
-            base.OnConfiguring(optionsBuilder);
         }
 
         public DbSet<Place> Places { get; set; }
