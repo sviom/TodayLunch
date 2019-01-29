9d364ef4b8e57ff507a669572ff2810ecf6d3471
HanByulKang <hanstar@designme.ai>
2019-01-29 12:33:26 +0900

    HomeController index 에러 수정

--- TodayLunchCore/Startup.cs
+++ TodayLunchCore/Startup.cs
@@ -16,8 +16,8 @@ namespace TodayLunchCore
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                //.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
         }
