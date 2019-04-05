c1f8f8331d51b7ef1f51caff88b89038921adaad
HanByulKang <kanghanstar@outlook.com>
2019-04-05 16:44:26 +0900

    WebHostBuilder에 KeyVault항목 추가

--- TodayLunchCore/Program.cs
+++ TodayLunchCore/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace TodayLunchCore
 {
@@ -11,15 +13,42 @@ namespace TodayLunchCore
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
+            //var host = new WebHostBuilder()
+            //    .UseKestrel()
+            //    .UseContentRoot(Directory.GetCurrentDirectory())
+            //    .UseIISIntegration()
+            //    .UseStartup<Startup>()
+            //    .UseApplicationInsights()
+            //    .Build();
 
-            host.Run();
+            //host.Run();
+
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => 
+            WebHost.CreateDefaultBuilder(args)
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .UseApplicationInsights()
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                // Build the current set of configuration to load values from
+                // JSON files and environment variables, including VaultName.
+                var builtConfig = config.Build();
+
+                // Use VaultName from the configuration to create the full vault URL.
+                var vaultUrl = $"https://{builtConfig["VaultName"]}.vault.azure.net/";
+
+                // Load all secrets from the vault into configuration. This will automatically
+                // authenticate to the vault using a managed identity. If a managed identity
+                // is not available, it will check if Visual Studio and/or the Azure CLI are
+                // installed locally and see if they are configured with credentials that can
+                // access the vault.
+                config.AddAzureKeyVault(vaultUrl);
+            })
+            .UseStartup<Startup>();
     }
 }
