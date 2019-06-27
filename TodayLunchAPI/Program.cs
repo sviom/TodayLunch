b84ef07a50e57dd0543f07cdff21ad4922e0f8c0
HanByulKang <kanghanstar@outlook.com>
2019-06-27 12:51:24 +0900

    API 용 프로젝트 추가

---
+++ TodayLunchAPI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TodayLunchAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                var builtConfig = config.Build();
+                var vaultUrl = $"https://{builtConfig["VaultName"]}.vault.azure.net/";
+                config.AddAzureKeyVault(vaultUrl);
+            })
+            .UseStartup<Startup>();
+    }
+}
