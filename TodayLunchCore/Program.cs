5cd17faf9ed3d76b1c86bf09fec4094824f42490
HanByulKang <kanghanstar@outlook.com>
2019-11-30 00:48:52 +0900

    key vault 관련 수정 중

--- TodayLunchCore/Program.cs
+++ TodayLunchCore/Program.cs
@@ -5,13 +5,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 
 namespace TodayLunchCore
 {
     public class Program
     {
+        private static string GetKeyVaultEndpoint() => "https://todaylunchkeyvault.vault.azure.net";
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -19,12 +24,21 @@ namespace TodayLunchCore
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                var keyVaultEndpoint = GetKeyVaultEndpoint();
+                if (!string.IsNullOrEmpty(keyVaultEndpoint))
+                {
+                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                    var keyVaultClient = new KeyVaultClient(
+                        new KeyVaultClient.AuthenticationCallback(
+                            azureServiceTokenProvider.KeyVaultTokenCallback));
+                    config.AddAzureKeyVault(keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                }
+            })
             .ConfigureWebHostDefaults(webBuilder =>
             {
-                webBuilder.ConfigureKestrel(servereOptions =>
-                {
-
-                }).UseStartup<Startup>();
+                webBuilder.UseStartup<Startup>();
             });
     }
 }
